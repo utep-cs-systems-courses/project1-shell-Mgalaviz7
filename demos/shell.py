fb6e41c8218d477d4dbc1e107d1f7c43a819e161
Mgalaviz7 <mgalaviz3@miners.utep.edu>
2020-09-15 17:36:26 -0600

    added ps1 check. added to pipe handler. removed command_handler. merged redirect and execute_command

--- demos/shell.py
+++ demos/shell.py
@@ -1,111 +1,134 @@
 #! /usr/bin/env python3
 
-import os, sys, time, re
+import os, sys, re, fileinput
 
-
+"""
+Main test to see if PS1 has been initalized while taking the input from the user and storing it
+as a variable that is passed to the input_handler.
+"""
 def main():
-
     while True:
-        
-        user_input = input(os.getcwd() + ">$ ")
+        if 'PS1' in os.environ: # check to see if PS1 has been initialized.
+            os.write(1, os.environ['PS1'].encode())
+        else:
+            user_input = input('$ ')
+        try:
+            user_input = input()
+        except EOFError:
+            sys.exit(1)
+
         input_handler(user_input)
+"""
+Input_handler designates what method will be called based on user_input.
 
 """
-Command handler takes in user input as a parameter and checks to see id the command enter is valid.
-Otherwise it will prompt that the user should input once again.
 
-"""        
-def command_handler(user_input):
-    try:
-        if os.system(user_input[0]) != 0:
-            print(user_input)
-            raise Exception()
-    except Exception as e:
-        print("\nCommand not found! Please Try again.\n")
-    finally:
-        pass
-
-"""
-Some functionaltiy still does not work properly
-
-"""            
 def input_handler(user_input):
 
     args = user_input.split()
 
-    command_handler(args)
-    
     if user_input == "":
         pass
-
-    if 'exit' in user_input:
+    
+    elif 'exit' in user_input:
         sys.exit(0)
-
-    if '>' in user_input or '<' in user_input:
-        redirect_handler(user_input)
-
-    if '|' in user_input:
-        user_input = user_input.split('|')
-        for args_input in user_input:
-            args = args_input.split()
-            command_handler(args)
-            execute_command(args)
-
-    if 'cd' in user_input:
-        os.chdir(args[1])
-        print(os.getcwd())
-
+        
+    elif '|' in user_input:
+        pipe_handler(user_input)        
+        
+    elif 'cd' in user_input:
+        try:
+            os.chdir(args[1].strip())
+        except FileNotFoundError:
+            pass
     else:
         execute_command(args)
+"""
+Pipe_handler takes in user input as a pramter. It then parses the input for two sperate commands.
+(Currently not working properly)
 
-        
-def redirect_handler(user_input):
-
-    
-    if '>' in user_input:
-        os.close(1)
-        args = user_input.split('>')
-        os.open(args[2], os.O_CREAT | os.O_WRONLY)
-        os.set_inheritable(1,True)
-        #command_execute(args)
-    else:
-        os.close(1)
-        args = user_input.split('<')
-        os.open(args[2], os.O_RDONLY)
-        os.set_inheritable(0,True)
-        #command_execute(args)
+"""
             
+def pipe_handler(user_input):
+    # child fork / creates a new process
+    rc = os.fork()
 
-def execute_command(args):
-        # create parents P_rocess ID_entification
-        pid = os.getpid()
-            
-        # child fork / creates a new process
-        rc = os.fork()
+    args = user_input.split('|')
+
+    if rc < 0:
+        sys.exit(1)
+
+    elif rc == 0:
+        pr, pw = os.pipe()
+        for f in (pr, pw):
+            os.set_inheritable(f, True)
         
-        # Verify that the fork is running
-        if rc < 0:
+        pf = os.fork()
+        
+        if pf < 0:                      # fork failed   
             sys.exit(1)
+    
+        elif pf == 0:                   #  child - will write to pipe
+            os.close(1)                 #  redirect child's stdout
+            os.dup(pw)
+            os.set_inheritable(1,True)
             
-        elif rc == 0: # child
-            # Get path Variable and splits it by the : which will provide individual paths.
-            # For each directory on the path we will walk though it and try to execve the program
-            # pointed to the filename            
-            #args = ["wc", "p3-exec.py"]
-            for dir in re.split(":", os.environ['PATH']): # try each directory in the path
-
-                program = "%s/%s" % (dir, args[0])
+            for fd in (pr,pw):    
+                os.close(fd)                    
+                print("\nhello from child\n")
+            execute_command(args[0].strip())
                 
-                try:
-                    os.execve(program, args, os.environ) # try to exec program
-                except FileNotFoundError:             # ...expected
-                    pass                              # ...fail quietly
-                
-            os.write(2, ("Could not execute %s\n" % args[0]).encode())
-            sys.exit(1)                 # terminate with error
-            
-        else:                           # parent (forked ok)
-            childPidCode = os.wait()
+        else:                           # parent (for
+            os.close(0)
+            os.dup(pr)
+            os.set_inheritable(0,True)
+            for fd in (pw,pr):
+                os.close(fd)
+                print("From child: <%s>" % line)
+            execute_command(args[1].strip())
+    
+    else:                           # parent (forked ok)
+        childPidCode = os.wait()
+"""
+Execute_command takes a pre parsed input that check to see if it is a redirect if so depending on
+the symbol it will either read from the last or create and write a new file. Followed by executing
+the the user input while traversing through the 'PATH' 
 
+"""        
+def execute_command(args):
+    pid = os.getpid()
+    
+    rc = os.fork()
+
+    if rc < 0:
+        sys.exit(1)
+
+    elif rc == 0:
+        if '>' in args or '<' in args:
+            if '>' in args:
+                args.remove(">")
+                os.close(1)
+                os.open(args[-1], os.O_CREAT | os.O_WRONLY);
+                os.set_inheritable(1, True)
+            else:
+                os.close(0)
+                sys.stdin = open(args[-1], 'r')
+                os.set_inheritable(0, True)
+        
+        # Get path Variable and splits it by the : which will provide individual paths.
+        # For each directory on the path we will walk though it and try to execve the program
+        # pointed to the filename            
+        for dir in re.split(":", os.environ['PATH']): # try each directory in the path
+            program = "%s/%s" % (dir, args[0])
+            try:
+                os.execve(program, args, os.environ) # try to exec program
+            except FileNotFoundError:             # ...expected
+                pass                              # ...fail quietly
+
+        os.write(2, ("Could not execute %s\n" % args[0]).encode())
+        sys.exit(1)                 # terminate with error
+
+
+        
 if __name__=="__main__":
     main()
