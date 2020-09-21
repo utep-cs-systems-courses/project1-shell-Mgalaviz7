699b7df34a8bde72842b6c2ea6a28fea00e3c5a8
Mgalaviz7 <mgalaviz3@miners.utep.edu>
2020-09-20 19:54:33 -0600

    fixed pipe method and added a pipe_execute method. Also tried to convert taking input using os.read()

--- demos/shell.py
+++ demos/shell.py
@@ -1,6 +1,6 @@
 #! /usr/bin/env python3
 
-import os, sys, re, fileinput
+import os, sys, re
 
 """
 Main test to see if PS1 has been initalized while taking the input from the user and storing it
@@ -11,28 +11,31 @@ def main():
         if 'PS1' in os.environ: # check to see if PS1 has been initialized.
             os.write(1, os.environ['PS1'].encode())
         else:
-            user_input = input('$ ')
+            os.write(1, ('$ ').encode())
         try:
-            user_input = input()
+            user_input = input() #os.read(0, 1024)
         except EOFError:
             sys.exit(1)
-
+            
+        #user_input = user_input.decode()
+        #user_input = user_input.split()
+        #print(user_input)
         input_handler(user_input)
+        
 """
 Input_handler designates what method will be called based on user_input.
 
 """
-
 def input_handler(user_input):
 
     args = user_input.split()
-
+    
     if user_input == "":
         pass
     
     elif 'exit' in user_input:
         sys.exit(0)
-        
+    
     elif '|' in user_input:
         pipe_handler(user_input)        
         
@@ -43,67 +46,74 @@ def input_handler(user_input):
             pass
     else:
         execute_command(args)
+        
 """
 Pipe_handler takes in user input as a pramter. It then parses the input for two sperate commands.
-(Currently not working properly)
+It then creates file descriptors for reading and writing for both parent and child pipes.
 
-"""
-            
+"""    
 def pipe_handler(user_input):
+
+    pr, pw = os.pipe()
+    
+    args = user_input.split('|')
+
     # child fork / creates a new process
     rc = os.fork()
 
-    args = user_input.split('|')
-
-    if rc < 0:
+    if rc < 0:                      # fork failed
         sys.exit(1)
 
-    elif rc == 0:
-        pr, pw = os.pipe()
-        for f in (pr, pw):
-            os.set_inheritable(f, True)
-        
-        pf = os.fork()
-        
-        if pf < 0:                      # fork failed   
-            sys.exit(1)
-    
-        elif pf == 0:                   #  child - will write to pipe
-            os.close(1)                 #  redirect child's stdout
-            os.dup(pw)
-            os.set_inheritable(1,True)
-            
-            for fd in (pr,pw):    
-                os.close(fd)                    
-                print("\nhello from child\n")
-            execute_command(args[0].strip())
-                
-        else:                           # parent (for
-            os.close(0)
-            os.dup(pr)
-            os.set_inheritable(0,True)
-            for fd in (pw,pr):
-                os.close(fd)
-                print("From child: <%s>" % line)
-            execute_command(args[1].strip())
-    
+    elif rc == 0:                   # child - will write to pipe
+        os.close(1)                 # redirect child's stdout
+        os.dup(pw)
+        os.set_inheritable(1, True)
+        for fd in (pr, pw):
+            os.close(fd)
+        command = args[0].split()   # first command
+        execute_pipe(command)
+        sys.exit(1)
     else:                           # parent (forked ok)
-        childPidCode = os.wait()
-"""
-Execute_command takes a pre parsed input that check to see if it is a redirect if so depending on
-the symbol it will either read from the last or create and write a new file. Followed by executing
-the the user input while traversing through the 'PATH' 
+        os.close(0)
+        os.dup(pr)
+        os.set_inheritable(0, True)
+        for fd in (pw, pr):
+            os.close(fd)
+        command = args[1].split()   # second command 
+        execute_pipe(command)
 
+"""
+Had to create this method beacsue it would double fork when run in execute_command method and 
+would cause the forkings and discriptors to not function properly. Method essentialy sets the path
+variable and splits it by the : which will provide individual paths.For each directory on the path
+we will walk though it and try to execve the program pointed to the filename 
+"""
+def execute_pipe(args):            
+
+    for dir in re.split(":", os.environ['PATH']): # try each directory in the path
+        program = "%s/%s" % (dir, args[0])
+        try:
+            os.execve(program, args, os.environ) # try to exec program
+        except FileNotFoundError:             # ...expected
+            pass                              # ...fail quietly    
+    os.write(2, ("Could not execute %s\n" % args[0]).encode())
+    sys.exit(1)                 # terminate with error
+ 
+"""
+Execute_command takes a pre parsed input that checks to see if it has a redirect symbol if so 
+depending on the symbol it will either read from the last input or create and write a new file. 
+Followed by executing the the user input while traversing through the 'PATH' 
 """        
 def execute_command(args):
     pid = os.getpid()
-    
+
     rc = os.fork()
 
     if rc < 0:
         sys.exit(1)
-
+        
     elif rc == 0:
+   
         if '>' in args or '<' in args:
             if '>' in args:
                 args.remove(">")
@@ -114,20 +124,20 @@ def execute_command(args):
                 os.close(0)
                 sys.stdin = open(args[-1], 'r')
                 os.set_inheritable(0, True)
-        
+            
         # Get path Variable and splits it by the : which will provide individual paths.
         # For each directory on the path we will walk though it and try to execve the program
-        # pointed to the filename            
+        # pointed to the filename
         for dir in re.split(":", os.environ['PATH']): # try each directory in the path
             program = "%s/%s" % (dir, args[0])
             try:
                 os.execve(program, args, os.environ) # try to exec program
             except FileNotFoundError:             # ...expected
                 pass                              # ...fail quietly
-
+            
         os.write(2, ("Could not execute %s\n" % args[0]).encode())
         sys.exit(1)                 # terminate with error
-
+        
 
         
 if __name__=="__main__":
